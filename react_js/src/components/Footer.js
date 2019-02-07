0e72868c7c5c562279f484948962d569fe83b228
Jaret Burkett <jaretburkett@gmail.com>
2019-02-07 06:30:15 -0600

    Cleanup and setup github page deploy

--- react_js/src/components/Footer.js
+++ react_js/src/components/Footer.js
@@ -5,12 +5,7 @@ class Footer extends Component {
     render() {
         return (
             <footer className="my-5 pt-5 text-muted text-center text-small">
-                <p className="mb-1">&copy; 2018 Jaret Burkett</p>
-                <ul className="list-inline">
-                    <li className="list-inline-item"><a href="#">Privacy</a></li>
-                    <li className="list-inline-item"><a href="#">Terms</a></li>
-                    <li className="list-inline-item"><a href="#">Support</a></li>
-                </ul>
+                <p className="mb-1">by Jaret Burkett</p>
             </footer>
         );
     }
